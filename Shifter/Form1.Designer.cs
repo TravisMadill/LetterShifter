e5993a2a99dfad55d09dc7b8b52426b2567135e9
TravisMadill <travismadill@gmail.com>
2018-06-29 20:03:19 -0400

    Initial commit

---
+++ Shifter/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Shifter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(266, 25);
+			this.numericUpDown1.Maximum = new decimal(new int[] {
+            26,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+			this.numericUpDown1.TabIndex = 0;
+			this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(263, 7);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(45, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Shift by:";
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox1.Location = new System.Drawing.Point(12, 51);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(374, 132);
+			this.richTextBox1.TabIndex = 2;
+			this.richTextBox1.Text = "";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox2.Location = new System.Drawing.Point(12, 208);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(374, 147);
+			this.richTextBox2.TabIndex = 3;
+			this.richTextBox2.Text = "";
+			this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 186);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Encoded:";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(54, 13);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Decoded:";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(398, 367);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.numericUpDown1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Shifter/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shifter
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			foreach (Control c in Controls)
+			{
+				if (c.Font.Equals(SystemFonts.DefaultFont))
+					c.Font = SystemFonts.MessageBoxFont;
+			}
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			richTextBox1_TextChanged(null, null);
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+			int spot = richTextBox1.SelectionStart;
+			richTextBox2.Text = letterShift(richTextBox1.Text, Convert.ToInt32(numericUpDown1.Value));
+			richTextBox1.SelectionStart = spot;
+			richTextBox1.SelectionLength = 0;
+		}
+
+		private void richTextBox2_TextChanged(object sender, EventArgs e)
+		{
+			int spot = richTextBox2.SelectionStart;
+			richTextBox1.Text = letterShift(richTextBox2.Text, -Convert.ToInt32(numericUpDown1.Value));
+			richTextBox2.SelectionStart = spot;
+			richTextBox2.SelectionLength = 0;
+		}
+
+		private string letterShift(string input, int shift)
+		{
+			StringBuilder msg = new StringBuilder();
+			if (input.Length > 0)
+			{
+				foreach (char c in input)
+				{
+					if (!(((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')) || ((c >= '0') && (c <= '9')) || ((c >= 'À') && (c <= 'ß')) || ((c >= 'à') && (c <= 'ÿ'))))
+						msg.Append(c);
+					else
+					{
+						int ltr = c;
+						if (c >= 'A' && c <= 'Z')
+						{
+							ltr -= 'A' - 'a';
+							ltr -= 'a';
+							ltr += shift + 26;
+							ltr %= 26;
+							ltr += 'a';
+							ltr += 'A' - 'a';
+						}
+						else if (c >= 'a' && c <= 'z')
+						{
+							ltr -= 'a';
+							ltr += shift + 26;
+							ltr %= 26;
+							ltr += 'a';
+						}
+						else if ((c >= 'À' && c <= 'ß') || (c >= 'à' && c <= 'ÿ'))
+						{
+							ltr -= 'À';
+							ltr += shift + 64;
+							ltr %= 64;
+							ltr += 'À';
+						}
+						else if (c >= '0' && c <= '9')
+						{
+							ltr -= '0';
+							ltr += shift + 10;
+							ltr %= 10;
+							ltr += '0';
+						}
+
+						msg.Append((char)ltr);
+					}
+				}
+			}
+			return msg.ToString();
+		}
+	}
+}
